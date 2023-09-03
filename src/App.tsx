0999191b9372c7c2957c0e7195fc5d756c551023
wp9975 <wojtek.pieczocs@gmail.com>
2023-09-03 16:13:31 +0200

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <h1 className='p-5 bg-slate-50 text-3xl text-center font-bold text-slate-900 '>React Typescript Template</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
